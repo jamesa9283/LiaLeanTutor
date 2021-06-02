c7e5ec574fc90e638faa2af35887d26a519d81a8
James Arthur <jamesarthur7@icloud.com>
2021-06-02 11:52:35 +0100

    added wk1 exercises

--- src/wk1/Exercises/exercises.lean
+++ src/wk1/Exercises/exercises.lean
@@ -6,12 +6,31 @@ variables P Q R : Prop
 
 example (h : P ∧ (Q ∧ R)) : (P ∧ Q) ∧ R :=
 begin
-  cases h with hp hqr,
-  cases hqr with hq hr,
-  split,
-  { exact and.intro hp hq },
-  { exact hr },
+  sorry
 end
 
-
 end question11
+
+section question6
+
+variables P Q R : Prop
+
+example : (P ∧ R) ∨ (Q ∧ R) → (P ∨ Q) ∧ R :=
+begin
+  sorry
+end
+
+end question6
+
+section question9
+
+variables A B C : Prop
+
+example (h : C → A ∨ B) : (C ∧ ¬A) → B :=
+begin
+  sorry
+end
+
+-- HINT: for this question, you may find it useful to to use `exfalso, contradiction` somewhere in the proof
+
+end question9
