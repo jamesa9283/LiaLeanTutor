dba8bf95227b64a5360bfe963adae6af3c20644c
James Arthur <jamesarthur7@icloud.com>
2021-07-02 11:32:55 +0100

    added wk3 stuff

--- src/wk3/lin_ind.lean
+++ src/wk3/lin_ind.lean
@@ -4,19 +4,42 @@ variables {ι : Type*} {R : Type*} {V' : Type*} {M : Type*}
 
 section one
 
+variables (a b : ℕ)
+#check ({a} : set ℕ)
+#check ({a, b} : set ℕ)
+
+
 variables {v : ι → M} [semiring R] [add_comm_monoid M] [module R M] {a b : R}
 
+#check false.elim
 lemma linear_independent_empty_type' (h : ¬ nonempty ι) : linear_independent R v :=
 begin
-  sorry
+  rw linear_independent_iff,
+  intros f hf,
+  ext i,
+  apply false.elim (h ⟨i⟩),
 end
+
+/-
+variables (A : ({a} : set ℕ))
+#check ⋃ (i : A), i
+-/
 
 lemma linear_independent_Union_of_directed' {η : Type*}
   {s : η → set M} (hs : directed (⊆) s)
   (h : ∀ i, linear_independent R (λ x, x : s i → M)) :
   linear_independent R (λ x, x : (⋃ i, s i) → M) :=
 begin
-  sorry
+  by_cases hη : nonempty η,
+  { resetI,
+    refine linear_independent_of_finite (⋃ i, s i) (λ t ht ft, _),
+    rcases set.finite_subset_Union ft ht with ⟨I, fi, hI⟩,
+    rcases hs.finset_le fi.to_finset with ⟨i, hi⟩,
+    exact (h i).mono (set.subset.trans hI $ set.bUnion_subset $
+      λ j hj, hi j (fi.mem_to_finset.2 hj)) },
+  { refine (linear_independent_empty _ _).mono _,
+    rintro _ ⟨_, ⟨i, _⟩, _⟩, exact hη ⟨i⟩ 
+  }
 end
 
 end one
@@ -27,9 +50,13 @@ open set module submodule
 
 variables [ring R] [add_comm_group M] [module R M]
 
+#check
+#check
+
 lemma exists_maximal_independent'' (s : ι → M) : ∃ I : set ι, linear_independent R (λ x : I, s x) ∧
   ∀ i ∉ I, ∃ a : R, a ≠ 0 ∧ a • s i ∈ span R (s '' I) :=
 begin
+  classical,
   sorry
 end
 
